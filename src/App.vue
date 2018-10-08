60189e5a71eea4cf11e52facfc5e4413eace7d63
Nikita Korba <no.korba@gmail.com>
2018-10-09 00:15:50 +0300

    Initial Commit. Video #24 Finished

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
+    <app-ninjas :ninjas="ninjas"></app-ninjas>
+    <hr>
+    <app-ninjas :ninjas="ninjas"></app-ninjas>
+    <app-footer v-bind:title="title"></app-footer>
+  </div>
+</template>
+
+<script>
+import HeaderVue from "./components/Header.vue";
+import FooterVue from "./components/Footer.vue";
+import NinjasVue from "./components/Ninjas.vue";
+
+export default {
+  components: {
+    "app-header": HeaderVue,
+    "app-footer": FooterVue,
+    "app-ninjas": NinjasVue
+  },
+  data() {
+    return {
+      ninjas: [
+        { name: "Ryu", speciality: "Vue Components", show: false },
+        { name: "Crystal", speciality: "HTML", show: false },
+        { name: "Hithoshi", speciality: "Events", show: false },
+        { name: "Tango", speciality: "Conditionals", show: false },
+        { name: "Kami", speciality: "Webpack", show: false },
+        { name: "Yoshi", speciality: "Data", show: false }
+      ],
+      title: "Vue Ninjas"
+    };
+  },
+  methods: {
+    updateTitle(event) {
+      this.title = event;
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer>
+    <p> {{ copyright }} {{ title }} </p>
+  </footer>
+</template>
+<script>
+  export default {
+    props: {
+      title: {
+        type: String
+      }
+    },
+    data() {
+      return {
+        copyright: `Copyright 2018 Nikita Korba`
+      }
+    }
+  }
+</script>
+<style scoped>
+  footer {
+    background: #222;
+    padding: 10px;
+  }
+
+  p {
+    color: lightgreen;
+    text-align: center;
+  }
+</style>
