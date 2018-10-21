35c2de252f876c6c676223870341396961d82fad
Nikita Korba <no.korba@gmail.com>
2018-10-22 00:10:50 +0300

    Video 34 Finished

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="show-blogs">
+    <div v-theme:column="'narrow'" class="show-blogs">
         <h1>All Blog Articles</h1>
         <div v-for="blog in blogs" class="single-blog">
-            <h2>{{ blog.title }}</h2>
+            <h2 v-rainbow>{{ blog.title }}</h2>
             <article> {{ blog.body }} </article>
         </div>
     </div>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Custom directives
+
+Vue.directive("rainbow", {
+  bind(elem, binding, vnode) {
+    elem.style.color = "#" + Math.random().toString().slice(2, 8);
+  }
+});
+
+Vue.directive("theme", {
+  bind(elem, binding, vnode) {
+    switch (binding.value) {
+      case "wide":
+        elem.style.maxWidth = "1200px";
+        break;
+
+      case "narrow":
+        elem.style.maxWidth = "560px";
+        break;
+    }
+    if (binding.arg === 'column') {
+      elem.style.padding = '20px';
+      elem.style.background = "#ddd";
+    }
+  }
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
