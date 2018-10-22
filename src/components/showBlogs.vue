bf147a87080294d011f4c44ccd07bd99ce6eff31
Nikita Korba <no.korba@gmail.com>
2018-10-22 20:20:00 +0300

    Video 37 Finished

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -2,8 +2,8 @@
     <div v-theme:column="'narrow'" class="show-blogs">
         <h1>All Blog Articles</h1>
       <input type="text" v-model="search" placeholder="Search blogs">
-        <div v-for="blog in filteredBlogs" class="single-blog">
-            <h2 v-rainbow>{{ blog.title | to-uppercase }}</h2>
+        <div v-for="blog in filteredBlogs" v-bind:key="blog.title" class="single-blog">
+            <h2 v-rainbow>{{ blog.title | toUppercase }}</h2>
             <article> {{ blog.body | snippet }} </article>
         </div>
     </div>
@@ -13,8 +13,8 @@
 export default {
   data() {
     return {
-        blogs: [],
-        search: ""
+      blogs: [],
+      search: ""
     };
   },
   computed: {
@@ -25,22 +25,37 @@ export default {
   methods: {},
   created() {
     fetch("https://jsonplaceholder.typicode.com/posts")
-    .then(res => res.json())
-    .then(res => this.blogs = res.slice(0,10));
-  } 
+      .then(res => res.json())
+      .then(res => (this.blogs = res.slice(0, 10)));
+  },
+  filters: {
+    toUppercase(val) {
+      return val.toUpperCase();
+    },
+    snippet(val) {
+      return val.slice(0, 100).concat("...");
+    }
+  },
+  directives: {
+    rainbow: {
+      bind(elem, binding, vnode) {
+        elem.style.color = "#" + Math.random().toString().slice(2, 8);
+      }
+    }
+  }
 };
 </script>
 
 <style>
 .show-blogs {
-    max-width: 800px;
-    margin: 0 auto;
+  max-width: 800px;
+  margin: 0 auto;
 }
 
 .single-blog {
-    padding: 20px;
-    margin: 20px 0;
-    box-sizing: border-box;
-    background: #eee;
+  padding: 20px;
+  margin: 20px 0;
+  box-sizing: border-box;
+  background: #eee;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,13 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 
 // Custom directives
-
-Vue.directive("rainbow", {
-  bind(elem, binding, vnode) {
-    elem.style.color = "#" + Math.random().toString().slice(2, 8);
-  }
-});
-
 Vue.directive("theme", {
   bind(elem, binding, vnode) {
     switch (binding.value) {
@@ -27,8 +20,6 @@ Vue.directive("theme", {
   }
 });
 
-Vue.filter("to-uppercase", val => val.toUpperCase());
-Vue.filter("snippet", val => val.slice(0, 100).concat("..."));
 new Vue({
   el: '#app',
   render: h => h(App)
