dc6adb8836e4fdacb95b264c457905cfb7335f8e
Nikita Korba <no.korba@gmail.com>
2018-10-28 19:26:38 +0200

    Video 42 Finished

---
+++ src/components/singleBlog.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="single-blog">
+      <h1>{{ blog.title }}</h1>
+      <article>
+          {{ blog.body }}
+      </article>
+  </div>  
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: this.$route.params.id,
+      blog: {}
+    };
+  },
+  created() {
+    fetch(`http://jsonplaceholder.typicode.com/posts/${this.id}`)
+      .then(res => res.json())
+      .then(res => {
+          console.log(res);
+          this.blog = res;
+          });
+  }
+};
+</script>
+
+<style scoped>
+#single-blog {
+    max-width: 960px;
+    margin: 0 auto;
+}
+</style>
